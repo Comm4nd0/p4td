310e6622773393d396a71240fdc534212a0fab6e
Marco Baldanza <marcobaldanza@hotmail.co.uk>
2026-02-06 18:18:03 +0000

    fix: enable signed s3 urls for media rendering

--- p4td_backend/settings.py
+++ p4td_backend/settings.py
@@ -172,10 +172,11 @@ if os.environ.get('AWS_STORAGE_BUCKET_NAME'):
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_DEFAULT_ACL = None  # Use bucket default ACL
     AWS_S3_FILE_OVERWRITE = False
+    AWS_QUERYSTRING_AUTH = True  # Enable signed URLs for private objects
     
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
@@ -187,7 +188,6 @@ if os.environ.get('AWS_STORAGE_BUCKET_NAME'):
             "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
     }
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 else:
     # Development: Use local file storage
     STORAGES = {
