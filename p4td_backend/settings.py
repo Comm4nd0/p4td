33ea3116054e18178934849381c8a3c01d93b304
Marco Baldanza <marcobaldanza@hotmail.co.uk>
2026-02-06 18:39:48 +0000

    Config: Use public region-specific S3 URLs

--- p4td_backend/settings.py
+++ p4td_backend/settings.py
@@ -172,11 +172,11 @@ if os.environ.get('AWS_STORAGE_BUCKET_NAME'):
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-1')
-    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-1.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_DEFAULT_ACL = None  # Use bucket default ACL
     AWS_S3_FILE_OVERWRITE = False
-    AWS_QUERYSTRING_AUTH = True  # Enable signed URLs
+    AWS_QUERYSTRING_AUTH = False  # Use public URLs
     
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
